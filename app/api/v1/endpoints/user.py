fd9a8e500ec4283727abe9ca236b711428b05faa
sourabhgrover <sourabhgrover29@gmail.com>
2025-07-07 16:52:28 +0530

    Add CRUD endpoints for user management

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -15,3 +15,19 @@ async def create_user_endpoint(user_create: UserCreate,db : AsyncDatabase = Depe
     if not new_user:
         raise HTTPException(status_code=500, detail="User could not be created due to a database error.")
     return new_user
+
+@router.get("/", summary="Get all users")
+async def get_all_users(db: AsyncDatabase = Depends(get_database)):
+        return {}
+
+@router.get("/{user_id}",summary="Get user by ID")
+async def get_user_by_id(user_id:str,db:AsyncDatabase = Depends(get_database)):
+    return {}
+
+@router.put("/{user_id}",summary="Update user by ID")
+async def update_user_by_id(user_id:str,data:dict,db:AsyncDatabase = Depends(get_database)):
+    return {}
+
+@router.delete("/${user_id}",summary="Delete user by ID")
+async def delete_user_by_id(user_id:str,db:AsyncDatabase= Depends(get_database)):
+    return {}
