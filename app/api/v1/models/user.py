2fee57cbe2652c5d40d8b2dfc01465bb046a46d6
sourabhgrover <sourabhgrover29@gmail.com>
2025-07-08 11:33:11 +0530

    User Model

--- app/api/v1/models/user.py
+++ app/api/v1/models/user.py
@@ -1,19 +1,39 @@
-from pydantic import BaseModel,  Field , BeforeValidator
+from pydantic import BaseModel,Field,BeforeValidator
 from typing import Annotated
-from datetime import datetime
+from enum import Enum
+from datetime import date,datetime
+from bson import ObjectId  # For MongoDB's native ObjectId
 
+# Custom type for ObjectId handling in Pydantic
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
+class GenderEnum(str,Enum):
+    MALE = "Male"
+    FEMALE = "Female"
+    OTHER = "Other"
+
 class UserBase(BaseModel):
-    firstname: str = Field(..., description="The user's first name", min_length=1)
-    lastname: str = Field(..., description="The user's last name", min_length=1)
+    first_name: Annotated[str, Field(min_length=3, max_length=50,description="The first name of the user", example="Sourabh")]
+    last_name: Annotated[str, Field(min_length=3, max_length=50,description="The last name of the user", example="Kumar")]
+    email: Annotated[str, Field(regex=r"^[\w\.-]+@[\w\.-]+\.\w+$", description="The email address of the user", example="")]
+    username: Annotated[str, Field(min_length=3, max_length=50, description="The username of the user", example="sourabhgrover")]
+    gender: Annotated[GenderEnum, Field(description="Gender of the user")]
+    dob: Annotated[date, Field(description="Date of birth of the user", example="1990-01-15")]
+    is_admin: Annotated[bool, Field(default=False, description="Indicates if the user is an admin", example=True)]
+    organization_id: Annotated[PyObjectId, Field(alias="organization_id", description="The ID of the organization the user belongs to")]
 
 class UserCreate(UserBase):
-    # Inherits all fields from UserBase
-    pass
+    password: Annotated[str,Field(min_length=8, max_length=128, description="Password for the user account",)] 
 
 class UserInDB(UserBase):
     """Model for users as stored in the database, including MongoDB _id and timestamps."""
-    id: PyObjectId = Field(alias="_id")
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    id: PyObjectId = Field(alias="_id", default_factory=lambda: str(ObjectId()))
+    hashed_password: Annotated[str, Field(description="Hashed password for the user account")]
+    created_at: datetime = Field(default_factory=date.utcnow)
+    updated_at: datetime = Field(default_factory=date.utcnow)
+
+    # class Config:
+    #     allow_population_by_field_name = True
+    #     json_encoders = {
+    #         str: lambda v: str(v)
+    #     }
